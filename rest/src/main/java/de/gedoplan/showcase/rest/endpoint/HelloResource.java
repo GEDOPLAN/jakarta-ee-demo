e0286d3865552d47a355a2df4f9184fa93910065
Markus Pauer <markus.pauer@gedoplan.de>
2023-01-19 20:23:22 +0100

    Hello-Endpunkt mit MP Config erweitert

--- rest/src/main/java/de/gedoplan/showcase/rest/endpoint/HelloResource.java
+++ rest/src/main/java/de/gedoplan/showcase/rest/endpoint/HelloResource.java
@@ -1,19 +1,24 @@
-package de.gedoplan.showcase.rest;
+package de.gedoplan.showcase.rest.endpoint;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("hello")
 @ApplicationScoped
 public class HelloResource {
+  @Inject
+  @ConfigProperty(name = "greeting.message", defaultValue = "Hello from nowhere")
+  private String message;
 
   @GET
   @Produces(MediaType.TEXT_PLAIN)
   public String get() {
-    return "Hello, RESTful Webservices";
+    return message;
   }
   
 }
